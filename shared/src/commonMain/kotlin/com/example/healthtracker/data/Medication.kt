a67251e9d71fd2ab532aebe80fcfac24c2ba0374
Kate Liu <katherineliu@slack-corp.com>
2024-12-29 13:07:14 -0500

    Add medication data class

---
+++ shared/src/commonMain/kotlin/com/example/healthtracker/data/Medication.kt
@@ -0,0 +1,15 @@
+package com.example.healthtracker.data
+
+import kotlinx.datetime.LocalTime
+
+data class Medication(
+    val name: String,
+    val dosage: String,
+    val schedule: List<LocalTime>,
+    val type: MedicationType
+)
+
+enum class MedicationType {
+    PRESCRIPTION,
+    SUPPLEMENT
+}
