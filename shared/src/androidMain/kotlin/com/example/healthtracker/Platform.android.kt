ff7196c0753c8259a68941b1cba3d3c08f4aefbf
Kate Liu <katherineliu@slack-corp.com>
2024-12-31 15:19:12 -0500

    Add back platform.kt

---
+++ shared/src/androidMain/kotlin/com/example/healthtracker/Platform.android.kt
@@ -0,0 +1,6 @@
+package com.example.healthtracker
+
+class AndroidPlatform : Platform {
+    override val name: String = "Android ${android.os.Build.VERSION.SDK_INT}"
+}
+actual fun getPlatform(): Platform = AndroidPlatform()
---
+++ shared/src/commonMain/kotlin/Platform.kt
@@ -0,0 +1,5 @@
+package com.example.healthtracker
+interface Platform {
+    val name: String
+}
+expect fun getPlatform(): Platform
