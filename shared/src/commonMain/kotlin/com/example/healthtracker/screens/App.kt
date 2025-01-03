2a1a0f3546ea64439b1f7121675fcc7d5f4d8a69
Kate Liu <katherineliu@slack-corp.com>
2025-01-02 22:02:58 -0500

    Clean up App with ContentScreen

--- shared/src/commonMain/kotlin/com/example/healthtracker/screens/App.kt
+++ shared/src/commonMain/kotlin/com/example/healthtracker/screens/App.kt
@@ -2,35 +2,12 @@ package com.example.healthtracker.screens
 
 import androidx.compose.runtime.Composable
 import com.example.healthtracker.HealthComponent
-import com.example.healthtracker.navigation.Screen
-import com.example.healthtracker.screens.bloodpressure.BloodPressureTrackerScreen
-import com.example.healthtracker.screens.bloodpressure.DashboardScreen
-import com.example.healthtracker.screens.welcome.WelcomeScreen
+import com.example.healthtracker.screens.medications.WeeklyCalendarView
+import com.example.healthtracker.screens.welcome.ContentScreen
 
 
 @Composable
 fun app(component: HealthComponent) {
-    when (component.currentScreen.value) {
-        Screen.WelcomeScreen -> WelcomeScreen(
-            component = component,
-            onNavigateToBloodPressure = {
-                component.navigateTo(Screen.Dashboard)
-            }
-        )
-        Screen.Dashboard -> DashboardScreen(
-            component = component,
-            viewModel = component.viewModel,
-            onAddReading = {
-                component.navigateTo(Screen.BloodPressureTracker)
-            }
-        )
-        Screen.BloodPressureTracker -> BloodPressureTrackerScreen(
-            component = component,
-            viewModel = component.viewModel,
-            onBackClick = {
-                component.navigateTo(Screen.Dashboard)
-            }
-        )
-    }
+//    WeeklyCalendarView(component = component)
+    ContentScreen(component = component)
 }
-
---
+++ shared/src/commonMain/kotlin/com/example/healthtracker/screens/welcome/ContentScreen.kt
@@ -0,0 +1,40 @@
+package com.example.healthtracker.screens.welcome
+
+import androidx.compose.runtime.Composable
+import com.example.healthtracker.HealthComponent
+import com.example.healthtracker.navigation.Screen
+import com.example.healthtracker.screens.bloodpressure.BloodPressureTrackerScreen
+import com.example.healthtracker.screens.bloodpressure.DashboardScreen
+import com.example.healthtracker.screens.medications.MedicationScreen
+
+@Composable
+fun ContentScreen(
+    component: HealthComponent
+) {
+    when (component.currentScreen.value) {
+        Screen.WelcomeScreen -> WelcomeScreen(
+            component = component,
+            onNavigateToBloodPressure = {
+                component.navigateTo(Screen.Dashboard)
+            }
+        )
+        Screen.Dashboard -> DashboardScreen(
+            component = component,
+            viewModel = component.viewBloodPressureModel,
+            onAddReading = {
+                component.navigateTo(Screen.BloodPressureTracker)
+            }
+        )
+        Screen.BloodPressureTracker -> BloodPressureTrackerScreen(
+            component = component,
+            viewModel = component.viewBloodPressureModel,
+            onBackClick = {
+                component.navigateTo(Screen.Dashboard)
+            }
+        )
+        Screen.MedicationScreen -> MedicationScreen(
+            component = component,
+            viewModel = component.viewMedicationModel,
+        )
+    }
+}
