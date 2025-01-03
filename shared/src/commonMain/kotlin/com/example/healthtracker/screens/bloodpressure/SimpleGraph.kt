d810fd607200594367a9a2df7d7283154539f160
Kate Liu <katherineliu@slack-corp.com>
2025-01-02 22:04:08 -0500

    Initial simple graph with koala plot

---
+++ shared/src/commonMain/kotlin/com/example/healthtracker/screens/bloodpressure/SimpleGraph.kt
@@ -0,0 +1,22 @@
+package com.example.healthtracker.screens.bloodpressure
+
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import io.github.koalaplot.core.util.ExperimentalKoalaPlotApi
+import io.github.koalaplot.core.ChartLayout
+
+@OptIn(ExperimentalKoalaPlotApi::class)
+@Composable
+fun SimpleChart() {
+    ChartLayout(
+        modifier = Modifier
+            .fillMaxWidth()
+            .height(200.dp)
+    ) {
+        Text("Test Chart")
+    }
+}
