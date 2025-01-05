6466f642777a66ad1392abf801240c193a5d5ddf
Kate Liu <katherineliu@slack-corp.com>
2025-01-05 06:57:54 -0500

    Allow for web view

--- shared/src/commonMain/kotlin/com/example/healthtracker/data/Article.kt
+++ shared/src/commonMain/kotlin/com/example/healthtracker/data/Article.kt
@@ -1,28 +1,34 @@
 package com.example.healthtracker.data
 
 import health_tracker.shared.generated.resources.Res
-import health_tracker.shared.generated.resources.blood_pressure
+import health_tracker.shared.generated.resources.bp
+import health_tracker.shared.generated.resources.bp2
+import health_tracker.shared.generated.resources.cold
+import health_tracker.shared.generated.resources.virus
 import org.jetbrains.compose.resources.DrawableResource
 
-data class Article(
-    val title: String,
-    val imageRes: DrawableResource,
-    val url: String)
+data class Article(val title: String, val imageRes: DrawableResource, val url: String)
 
-val articles = listOf(
+val articles =
+  listOf(
     Article(
-        title = "Learn about High Blood Pressure",
-        imageRes = Res.drawable.blood_pressure,
-        url = "https://www.cdc.gov/high-blood-pressure/about/index.html"
+      title = "Learn about High Blood Pressure",
+      imageRes = Res.drawable.bp,
+      url = "https://www.cdc.gov/high-blood-pressure/about/index.html",
     ),
     Article(
-        title = "Learn about High Blood Pressure",
-        imageRes = Res.drawable.blood_pressure,
-        url = "https://www.cdc.gov/high-blood-pressure/about/index.html"
+      title = "What your BP Reading Means",
+      imageRes = Res.drawable.bp2,
+      url = "https://www.mayoclinic.org/diseases-conditions/high-blood-pressure/in-depth/blood-pressure/art-20050982",
     ),
     Article(
-        title = "Learn about High Blood Pressure",
-        imageRes = Res.drawable.blood_pressure,
-        url = "https://www.cdc.gov/high-blood-pressure/about/index.html"
-    )
-)
+      title = "Prep for the Winter",
+      imageRes = Res.drawable.cold,
+      url = "https://www.cdc.gov/winter-weather/safety/index.html",
+    ),
+    Article(
+      title = "Prevent Norovius",
+      imageRes = Res.drawable.virus,
+      url = "https://www.cdc.gov/norovirus/prevention/index.html",
+    ),
+  )
--- shared/src/commonMain/kotlin/com/example/healthtracker/screens/welcome/ArticleCard.kt
+++ shared/src/commonMain/kotlin/com/example/healthtracker/screens/welcome/ArticleCard.kt
@@ -3,8 +3,12 @@ package com.example.healthtracker.screens.welcome
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.ui.Alignment
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.aspectRatio
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
@@ -14,44 +18,46 @@ import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.dp
 import com.example.healthtracker.data.Article
 import health_tracker.shared.generated.resources.Res
-import health_tracker.shared.generated.resources.blood_pressure
+import health_tracker.shared.generated.resources.bp
 import org.jetbrains.compose.resources.painterResource
 
 @Composable
-fun ArticleCard(article: Article) {
-    Card(
-        modifier = Modifier
-            .fillMaxWidth()
-            .height(150.dp)
-            .clickable { /* Handle card click */ },
-        colors = CardDefaults.cardColors(MaterialTheme.colorScheme.surface),
-        elevation = CardDefaults.elevatedCardElevation()
-    ) {
-        Row(modifier = Modifier.padding(16.dp)) {
-            Image(
-                painter = painterResource(Res.drawable.blood_pressure),
-                contentDescription = article.title,
-                modifier = Modifier
-                    .size(100.dp)
-                    .padding(end = 16.dp)
-            )
-            Column(
-                verticalArrangement = Arrangement.Center,
-                horizontalAlignment = Alignment.Start
-            ) {
-                Text(
-                    text = article.title,
-                    style = MaterialTheme.typography.bodyLarge,
-                    maxLines = 2,
-                    overflow = TextOverflow.Ellipsis
-                )
-            }
-        }
+fun ArticleCard(article: Article, onArticleClick: (String) -> Unit) {
+  Card(
+    modifier = Modifier.fillMaxWidth().aspectRatio(1f).clickable { onArticleClick(article.url) },
+    colors = CardDefaults.cardColors(MaterialTheme.colorScheme.surface),
+    elevation = CardDefaults.elevatedCardElevation(),
+  ) {
+    Box(modifier = Modifier.fillMaxSize()) {
+      Image(
+        painter = painterResource(article.imageRes),
+        contentDescription = article.title,
+        contentScale = ContentScale.Crop,
+        modifier = Modifier.fillMaxSize(),
+      )
+      Column(verticalArrangement = Arrangement.Center, horizontalAlignment = Alignment.CenterHorizontally) {
+        Text(
+          text = article.title,
+          style = MaterialTheme.typography.bodyLarge.copy(
+            fontWeight = FontWeight.Bold,
+            color = Color.Black
+          ),
+          modifier = Modifier.padding(10.dp),
+          textAlign = TextAlign.Center,
+          maxLines = 2,
+          overflow = TextOverflow.Ellipsis
+        )
+
+      }
     }
+  }
 }
