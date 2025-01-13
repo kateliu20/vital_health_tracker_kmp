c1b361e1e79dbf151272c5a4b6f173c9c3c9ee71
Kate Liu <katherineliu@slack-corp.com>
2025-01-13 00:29:30 -0800

    Add libraries used and fixing whitespace

--- README.md
+++ README.md
@@ -5,4 +5,9 @@ Vital is a comprehensive app designed to help individuals track and manage their
 
 The app features curated articles from the CDC on blood pressure and general health topics, conveniently displayed on the home screen. Additionally, Vital offers a robust set of tools, including a dashboard for insights, a "medicine cabinet" for tracking medications, and a calendar for managing appointments or journaling health-related events.
 
-[demo_health_tracker.mp4](shared/src/commonMain/composeResources/demo_health_tracker.mp4)
+## Libraries Used
+- **[Compose WebView Multiplatform](https://github.com/KevinnZou/compose-webview-multiplatform)**: Providing basic webview functionalities. Used for tha article cards on the welcome screen.
+- **[Koala Plot](https://github.com/KoalaPlot/koalaplot-core)**: A library for data visualization, used for the blood pressure graphing.
+- **[Compose Multiplatform Date Time Picker](https://github.com/Chaintech-Network/compose_multiplatform_date_time_picker)**: A Compose-based date-time picker. Used for scheduling appointments.
+- **[UUID](https://github.com/benasher44/uuid)**: A library for generating UUIDs. Used for generating unique IDs to add or delete items.
+- **[Spotless](https://github.com/diffplug/spotless)**: Code formatting plugin.
--- shared/build.gradle.kts
+++ shared/build.gradle.kts
@@ -1,90 +1,90 @@
 import org.jetbrains.kotlin.gradle.dsl.JvmTarget
 
 plugins {
-  alias(libs.plugins.kotlinMultiplatform)
-  alias(libs.plugins.androidLibrary)
-  alias(libs.plugins.compose)
-  alias(libs.plugins.kotlin.plugin.compose)
-  alias(libs.plugins.spotless)
-  alias(libs.plugins.sqldelight)
+    alias(libs.plugins.kotlinMultiplatform)
+    alias(libs.plugins.androidLibrary)
+    alias(libs.plugins.compose)
+    alias(libs.plugins.kotlin.plugin.compose)
+    alias(libs.plugins.spotless)
+    alias(libs.plugins.sqldelight)
 }
 
 spotless {
-  kotlin {
-    target("src/**/*.kt")
-    ktfmt().googleStyle()
-    trimTrailingWhitespace()
-    endWithNewline()
-  }
-  kotlinGradle {
-    target("*.gradle.kts")
-    ktfmt().googleStyle()
-    trimTrailingWhitespace()
-    endWithNewline()
-  }
+    kotlin {
+        target("src/**/*.kt")
+        ktfmt().googleStyle()
+        trimTrailingWhitespace()
+        endWithNewline()
+    }
+    kotlinGradle {
+        target("*.gradle.kts")
+        ktfmt().googleStyle()
+        trimTrailingWhitespace()
+        endWithNewline()
+    }
 }
 
 kotlin {
-  androidTarget {
-    compilations.all {
-      compileTaskProvider.configure { compilerOptions { jvmTarget.set(JvmTarget.JVM_1_8) } }
+    androidTarget {
+        compilations.all {
+            compileTaskProvider.configure { compilerOptions { jvmTarget.set(JvmTarget.JVM_1_8) } }
+        }
     }
-  }
 
-  listOf(iosX64(), iosArm64(), iosSimulatorArm64()).forEach {
-    it.binaries.framework {
-      baseName = "shared"
-      isStatic = true
+    listOf(iosX64(), iosArm64(), iosSimulatorArm64()).forEach {
+        it.binaries.framework {
+            baseName = "shared"
+            isStatic = true
+        }
     }
-  }
 
-  sourceSets {
-    commonMain.dependencies {
-      implementation(compose.runtime)
-      implementation(compose.foundation)
-      implementation(compose.material3)
-      implementation(compose.ui)
-      implementation(compose.components.resources)
+    sourceSets {
+        commonMain.dependencies {
+            implementation(compose.runtime)
+            implementation(compose.foundation)
+            implementation(compose.material3)
+            implementation(compose.ui)
+            implementation(compose.components.resources)
 
-      api(libs.compose.webview.multiplatform)
+            api(libs.compose.webview.multiplatform)
 
-      implementation(libs.kotlinx.datetime)
-      implementation(libs.koalaplot.core)
-      implementation(libs.kmp.date.time.picker)
-      implementation(libs.kmp.uiud)
+            implementation(libs.kotlinx.datetime)
+            implementation(libs.koalaplot.core)
+            implementation(libs.kmp.date.time.picker)
+            implementation(libs.kmp.uiud)
 
-      implementation(libs.sqldelight.coroutines)
-      implementation(libs.sqldelight.async)
+            implementation(libs.sqldelight.coroutines)
+            implementation(libs.sqldelight.async)
+        }
+        commonTest.dependencies { implementation(libs.kotlin.test) }
+
+        androidMain.dependencies { implementation(libs.sqldelight.driver.android) }
+
+        iosMain.dependencies { implementation(libs.sqldelight.driver.native) }
     }
-    commonTest.dependencies { implementation(libs.kotlin.test) }
-
-    androidMain.dependencies { implementation(libs.sqldelight.driver.android) }
-
-    iosMain.dependencies { implementation(libs.sqldelight.driver.native) }
-  }
 }
 
 android {
-  namespace = "com.example.healthtracker"
-  compileSdk = 35
-  defaultConfig { minSdk = 24 }
-  compileOptions {
-    sourceCompatibility = JavaVersion.VERSION_1_8
-    targetCompatibility = JavaVersion.VERSION_1_8
-  }
+    namespace = "com.example.healthtracker"
+    compileSdk = 35
+    defaultConfig { minSdk = 24 }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
 }
 
 dependencies {
-  implementation(libs.androidx.runtime.android)
-  implementation(libs.compose.material3)
-  implementation(libs.androidx.core.i18n)
+    implementation(libs.androidx.runtime.android)
+    implementation(libs.compose.material3)
+    implementation(libs.androidx.core.i18n)
 }
 
 sqldelight {
-  databases {
-    create("BloodPressure") {
-      packageName.set("com.example.healthtracker.sqldelight")
-      generateAsync.set(true)
+    databases {
+        create("BloodPressure") {
+            packageName.set("com.example.healthtracker.sqldelight")
+            generateAsync.set(true)
+        }
     }
-  }
 }
